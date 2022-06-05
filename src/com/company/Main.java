93cf9d4c608930d725e984e946e6879a4b73f1ee
qkrxodud00 <qkrxodud00@gmail.com>
2022-06-05 17:37:48 +0900

    *가장 먼 노드

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,48 +1,60 @@
 package com.company;
 
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
+
+    static int [][] arr;
+    static int[] checked; //확인 여부
+
     public static void main(String [] args) {
         Main main = new Main();
-        String skill = "CBD";
-        String [] skill_trees = {"BACDE", "CBADF", "AECB", "BDA"};
-        System.out.println(main.solution(skill, skill_trees));
+        int n = 6;
+        int [][] edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+        System.out.println(main.solution(n, edge));
     }
 
-    public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        Queue<Character> Q = new LinkedList<>();
-        for (char skillValue : skill.toCharArray()) {
-            Q.offer(skillValue);
+    public int solution(int n, int[][] edge) {
+        arr = new int [n+1][n+1];
+        checked = new int [n+1];
+
+        for (int i = 0; i <= n; i++) {
+            arr[edge[i][0]][edge[i][1]] = 1;
+            arr[edge[i][1]][edge[i][0]] = 1;
         }
+        return BFS( 1, n);
+    }
 
-        for (String skill_tree : skill_trees) {
-            char [] skillArr = skill_tree.toCharArray();
-            boolean flag = true;
+   public static int BFS( int start, int n) {
+        Queue<Integer> Q = new LinkedList<>();
+        Q.offer(start);
+        checked[start] = 1;
 
-            for (char c : skillArr) {
-                if (Q.contains(c)) {
-                    if (Q.peek() != c) {
-                        flag = false;
-                        break;
-                    } else {
-                        Q.poll();
+        int L = 1 ;
+        int levelMax = 0;
+        int answer = 0;
+
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++){
+                int cur = Q.poll();
+                for (int j = 1; j <=n; j++) {
+                    if (arr[cur][j] == 1 && checked[j] != 1) {
+                        if (levelMax < L) {
+                            answer = 0;
+                        }
+                        levelMax = Math.max(levelMax, L);
+                        answer++;
+                        checked[j] = 1;
+                        Q.offer(j);
                     }
                 }
             }
-
-            if (flag == true) answer++;
-
-            Q.removeAll(Q);
-            for (char skills : skill.toCharArray()) {
-                Q.offer(skills);
-            }
+            L++;
         }
-        return answer;
-    }
 
+        return answer;
+
+    }
 
 }
