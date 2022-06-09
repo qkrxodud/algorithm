9c52df02b0f700124e8c2858dbc777d6ffd478a4
qkrxodud00 <qkrxodud00@gmail.com>
2022-06-09 18:46:33 +0900

    *프로그래머스 가장 큰 수

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,49 @@
 package com.company;
 
-import java.util.HashMap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
+    static int[] numbers;
+    static ArrayList<Integer> list = new ArrayList<>();
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Main main = new Main();
-        String [] phone_book = {"119", "97674223", "1195524421"};
-        System.out.println(main.solution(phone_book));
+        numbers = new int[]{3, 30, 601, 621 , 0 ,300, 301, 34, 5, 9};
+        for (int i : numbers) {
+            list.add(i);
+        }
+        System.out.println(main.solution());
     }
 
-    public boolean solution(String[] phone_book) {
-        HashMap<String, Integer> map = new HashMap<>();
+    public String solution() {
+        String answer = "";
+        ArrayList<String> strNumbers = new ArrayList();
+        // int 형을 String 으로 변환한다.
+        for(Integer num : numbers){
+            strNumbers.add(String.valueOf(num));
+        }
 
-        for (int i = 0; i < phone_book.length; i++)
-            map.put(phone_book[i], i);
+        // 문장의 숫자를 뒷 배열 + 앞 배열을 합친 것과 앞 배열 + 뒷 배열을 합친값을 비교
+        Collections.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int temp = (o2+o1).compareTo(o1+o2);
+                return temp;
 
-        for (int i = 0; i < phone_book.length; i++)
-            for (int j = 0; j < phone_book[i].length(); j++) {
-                String temp = phone_book[i].substring(0, j);
-                if (map.containsKey(temp))
-                    return false;
             }
-        return true;
+        });
+
+        if(strNumbers.get(0).startsWith("0")) return "0";
+        for (String s : strNumbers) {
+            answer+=s;
+        }
+        return answer;
 
     }
-
 
 }
+
+
